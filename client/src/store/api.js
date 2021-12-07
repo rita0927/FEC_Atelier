b81d57e1d5179d43955ac9a7822a89bf3071fb01
Yu Zhang <rita890927@gmail.com>
2021-12-07 18:10:24 -0500

    add deployment comment

--- client/src/store/api.js
+++ client/src/store/api.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 const port = 3000;
+
+//localhost:${port} needs to be updated to the public url for deployment
 const baseURL = `http://localhost:${port}/api/`;
 
 const makeApiCall = (method, url, data) => axios.request({ baseURL, url, method, data });
